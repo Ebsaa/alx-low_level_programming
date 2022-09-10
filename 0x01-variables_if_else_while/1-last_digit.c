c97aa208fb8623e3cee4f1a4b3c309f3f22761e3
Ebsaa <ebsaumar200@gmail.com>
2022-09-09 18:18:36 -0700

    c1 task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,26 +2,28 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*main-print if num is positive, zero or negative
+*main-print num to var
 *
 *Return:Always 0 (Succesful)
 */
 int main(void)
 {
 int n;
+char last[] = "last digit of";
 srand(time(0));
-n = rand() - RAND_MAX / 2
-if (n > 0)
+n = rand() - RAND _MAX / 2;
+printf("%s %d is %d and is ", last, n, n % 10);
+if (n % 10 > 5)
 {
-printf("%d is positive\n", n);
+printf("greater than 5\n");
 }
-else if (n == 0)
+else if (n % 10 == 0)
 {
-printf("%d is zero\n", n);
+printf("0\n");
 }
 else
 {
-printf("%d is negative\n", n);
+printf("less than 6 and not 0\n");
 }
 return (0);
 }
