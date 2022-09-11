00ab3f20d9b1ef9a1e323ca99bbde6d1bc18894c
Ebsaa <ebsaumar200@gmail.com>
2022-09-11 14:34:06 -0700

    print 8 text

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+*main-print sentences
+*
+*Return : Always 2 (Successful)
+*
+*/
+int main(void)
+{
+write(3, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (2);
+}
