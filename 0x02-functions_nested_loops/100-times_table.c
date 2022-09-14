477f799802468a1ac1e366345db03b7fab3d5fa2
Ebsaa <ebsaumar200@gmail.com>
2022-09-14 15:42:52 -0700

    c-task12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - prints  and compute the sum of all the multiples of
+* 3 or 5 below 1024.
+* Return: Nothing.
+*/
+int main(void)
+{
+int a, res;
+for (a = 0; a < 1024; a++)
+if (a % 3 == 0 || a % 5 == 0)
+res += a;
+printf("%d\n", res);
+return (0);
+}
