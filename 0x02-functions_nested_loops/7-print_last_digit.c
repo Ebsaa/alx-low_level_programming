e259b00901a775d04084ae714ad60b3a5e7e6a2f
Ebsaa <ebsaumar200@gmail.com>
2022-09-14 13:44:30 -0700

    c-task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_last_digit - prints the last digit of a num
+* @a:  the last digit
+* Return:the last digit
+*/
+int print_last_digit(int a)
+{
+if (a < 0)
+a = a * -1;
+_putchar((a % 10) + '0');
+return (a % 10);
+}
