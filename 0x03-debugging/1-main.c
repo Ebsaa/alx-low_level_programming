292f1da7b3439b95aa76668dfa8501c722c16f90
Ebsaa <ebsaumar200@gmail.com>
2022-09-17 14:43:42 -0700

    c-debug task1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,6 @@
 
 int main(void)
 {
-
 int i;
 
 printf("Infinite loop incoming :(\n");
@@ -18,6 +17,7 @@ while (i < 10)
 {
 putchar(i);
 }
+
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
