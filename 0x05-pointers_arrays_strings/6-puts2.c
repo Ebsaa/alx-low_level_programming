a9f2fb6a67f6bbc73a545064c778888484e48f81
Ebsaa <ebsaumar200@gmail.com>
2022-09-19 16:57:47 -0700

    pointerc-task 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,17 +4,18 @@
 * puts2 - Prints out every other char
 * @str: input string to print
 */
-
 void puts2(char *str)
 {
 
-int a;
-
-while (str[a] != '\0')
+int len, a;
+len = 0;
+while (str[len] != '\0')
 {
-if (a % 2 == 0)
+len++;
+}
+for (a = 0; a < len; a += 2)
+{
 _putchar(str[a]);
-a++;
 }
 _putchar('\n');
 }
