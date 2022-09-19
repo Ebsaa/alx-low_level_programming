5acb1aab1da3b5ca89f9fcdf1257ffb816c281c5
Ebsaa <ebsaumar200@gmail.com>
2022-09-18 17:38:05 -0700

    c4-5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* more_numbers - prints (0-14) 10 times
+* followed by a new line
+*/
+void more_numbers(void)
+{
+
+int a, b;
+
+for (a = 0; a < 10; a++)
+{
+for (b = 0; b < 15; b++)
+{
+if (b >= 10)
+_putchar(b / 10 + '0');
+_putchar(b % 10 + '0');
+}
+_putchar('\n');
+}
+}
