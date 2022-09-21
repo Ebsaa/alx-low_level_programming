edf889dc09d4bfa908f07a83127ab058b7c5a700
Ebsaa <ebsaumar200@gmail.com>
2022-09-21 14:50:40 -0700

    Task-0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,21 @@
 
 char *_strcat(char *dest, char *src)
 {
-int index = 0;
-dest_len = 0;
-while (dest[index++])
-dest_len++;
-for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index];
+int a, b;
+
+a = 0;
+b = 0;
+
+while (dest[a] != '\0')
+a++;
+
+while (src[b] != '\0')
+{
+dest[a] = src[b];
+b++;
+a++;
+}
+dest[a] = '\0';
+
 return (dest);
 }
