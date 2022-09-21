a033190d20b9d491fc26f0d848af3c0fa537d3db
Ebsaa <ebsaumar200@gmail.com>
2022-09-21 16:02:04 -0700

    Task-c 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,12 +11,10 @@
 char *_strncat(char *dest, char *src, int n);
 
 {
-int a, b;
-a = 0;
-for (b = 0; dest[b] != '\0'; b++)
-while (src[a] != '\0' && a < n)
-dest[b + a] = src[a];
-a++;
-
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
 return (dest);
 }
