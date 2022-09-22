af7a27b8c470f07a7d9f893eca8ff21fd9ea0faf
Ebsaa <ebsaumar200@gmail.com>
2022-09-22 05:39:09 -0700

    task 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strncpy - function that copy the string
+* if length of src stringis less than max byte num &
+* remaider dest string is less than max byte num
+* @dest: buffer storing string cpy
+* @src: source string
+* @n: max num byte copied
+* Return: return
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for (i = 0; i < n; i++)
+dest[i] = '\0';
+return (dest);
+}
