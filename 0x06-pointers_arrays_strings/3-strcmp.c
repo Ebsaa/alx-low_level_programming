072c4370748c74d6efa0d1c371a01c0630656f05
Ebsaa <ebsaumar200@gmail.com>
2022-09-22 06:29:14 -0700

    task 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* _strcmp - function that compare two strings
+* @s1: 1st string
+* @s2: 2nd string
+* Return: 0 if s1 == s2, -ve if s1 < s2 & +ve if s1 > s2
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+
+int a = 0;
+diff = 0;
+while (1)
+{
+if (s1[a] == '\0' && s2[a] == '\0')
+break;
+else if (s1[a] == '\0')
+{
+diff = s2[a];
+break;
+}
+else if (s2[a] == '\0')
+{
+diff = s1[a];
+break;
+}
+else if (s1[a] != s2[a])
+{
+diff = s1[a] - s2[a];
+break;
+}
+else
+i++;
+return (diff);
+}
