e3e9eddf0290600e1bc9481fbb4be2c1e52597d0
Ebsaa <ebsaumar200@gmail.com>
2022-09-23 13:47:16 -0700

    main-file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,6 +10,7 @@
 
 void reverse_array(int *a, int n);
 void print_number(int n);
+int _putchar(char c);
 int _strcmp(char *s1, char *s2);
 void print_buffer(char *b, int size);
 char *_strcat(char *dest, char *src);
