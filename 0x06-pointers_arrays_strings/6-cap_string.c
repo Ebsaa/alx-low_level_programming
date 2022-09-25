a019379f9d6bd772e4cc80bfce5fbceea8b71d48
Ebsaa <ebsaumar200@gmail.com>
2022-09-25 14:22:33 -0700

    task6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,40 +8,31 @@
 char *cap_string(char *str)
 
 {
-int i = 0, j, k;
+char i, j, k;
 
 char delimiters[] = " \t\n,;.!?\"(){}";
-
 for (; str[i]; i++)
 {
-
 	if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
 	{
 		str[i] -= 32;
 	}
 	j = 0;
-for (; delimiters[j]; j++)
+	for (; delimiters[j]; j++)
 	{
-	if (str[i] == delimiters[j])
+			if (str[i] == delimiters[j])
 		{
-			i++;
-for (k = 0; delimiters[k]; k++)
-
-		{
-	if (str[i] == delimiters[k])
-
-				{
-
 				i++;
-
-				}
-
-		}
-
-	if (str[i] >= 'a' && str[i] <= 'z')
-
+				for (k = 0; delimiters[k]; k++)
 			{
-		str[i] -= 32;
+					if (str[i] == delimiters[k])
+				{
+					i++;
+				}
+			}
+					if (str[i] >= 'a' && str[i] <= 'z')
+			{
+						str[i] -= 32;
 			}
 		}
 	}
