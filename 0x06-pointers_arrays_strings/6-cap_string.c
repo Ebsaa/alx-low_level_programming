77380aae395a77e35e244f09d0a2dd9ad594d7f8
Ebsaa <ebsaumar200@gmail.com>
2022-09-25 14:35:58 -0700

    task--6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,34 +8,34 @@
 char *cap_string(char *str)
 
 {
-int i, j, k;
+int i = 0, j, k;
 
 char delimiters[] = " \t\n,;.!?\"(){}";
 for (; str[i]; i++)
 {
-	if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
-	{
-		str[i] -= 32;
-	}
-	j = 0;
-	for (; delimiters[j]; j++)
-	{
-			if (str[i] == delimiters[j])
-		{
-				i++;
-				for (k = 0; delimiters[k]; k++)
-			{
-					if (str[i] == delimiters[k])
-				{
-					i++;
-				}
-			}
-					if (str[i] >= 'a' && str[i] <= 'z')
-			{
-						str[i] -= 32;
-			}
-		}
-	}
+if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
+{
+str[i] -= 32;
+}
+j = 0;
+for (; delimiters[j]; j++)
+{
+if (str[i] == delimiters[j])
+{
+i++;
+for (k = 0; delimiters[k]; k++)
+{
+if (str[i] == delimiters[k])
+{
+i++;
+}
+}
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] -= 32;
+}
+}
+}
 }
 return (str);
 }
