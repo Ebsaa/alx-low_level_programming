64605cb48d5475ad6a00decb25e4631587a275b0
Ebsaa <ebsaumar200@gmail.com>
2022-09-29 05:53:41 -0700

    task 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,41 +1,28 @@
 #include "main.h"
 
 /**
-* _strlen_recursion - function return the length of a string
-* @s: the string
-* Return: length of s
-*/
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
-}
-
-/**
-* palcheck - checker
-* @x: iterration
-* @y: length of s
-* @s: string
+* prime_check - confirm if s is the sqrt of c
+* @s: parameter
+* @c: number to find if it is a prime numberr
 * Return: 1 or 0
 */
-int palcheck(int x, int y, char *s)
+int prime_check(int s, int c)
 {
-	if (x >= y)
-		return (1);
-	if (*(s + x) != *(s + y - x))
-		return (0);
-	return (palcheck(x + 1, y, s));
+if (s * s > c)
+return (1);
+if (c % s == 0)
+return (0);
+return (prime_check(s + 1, c));
 }
 
 /**
-* is_palindrome - verify if a string is a palindrome
-* @s: string
-* Return: 1 if s is a palindrome otherwise 0
+* is_prime_number - function checks if n is a prime number
+* @n: parameter
+* Return: 1 if n is prime number or 0 else
 */
-int is_palindrome(char *s)
+int is_prime_number(int n)
 {
-	int i = _strlen_recursion(s);
-	return (palcheck(0, i - 1, s));
+if (n <= 1)
+return (0);
+return (prime_check(2, n));
 }
