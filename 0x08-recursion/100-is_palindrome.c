4db67207e036a0f72d636f30c9fc79538d7a9c98
Ebsaa <ebsaumar200@gmail.com>
2022-09-28 17:22:04 -0700

    task 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,10 +7,10 @@
 */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
+if (*s == '\0')
+return (0);
+else
+return (1 + _strlen_recursion(s + 1));
 }
 
 /**
@@ -22,11 +22,11 @@ int _strlen_recursion(char *s)
 */
 int palcheck(int x, int y, char *s)
 {
-	if (x >= y)
-		return (1);
-	if (*(s + x) != *(s + y - x))
-		return (0);
-	return (palcheck(x + 1, y, s));
+if (x >= y)
+return (1);
+if (*(s + x) != *(s + y - x))
+return (0);
+return (palcheck(x + 1, y, s));
 }
 
 /**
@@ -36,6 +36,6 @@ int palcheck(int x, int y, char *s)
 */
 int is_palindrome(char *s)
 {
-	int i = _strlen_recursion(s);
-	return (palcheck(0, i - 1, s));
+int i = _strlen_recursion(s);
+return (palcheck(0, i - 1, s));
 }
