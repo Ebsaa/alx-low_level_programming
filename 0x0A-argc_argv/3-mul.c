c2eb17cb76cedae9fe52f90ae2d1008498f1a2fc
Ebsaa <ebsaumar200@gmail.com>
2022-09-30 15:49:58 -0700

    task 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - multiplies two numbers
+* @argc: number of arguments passed to the function
+* @argv: argument vector of pointers to strings
+* Return: 1 if errors, else 0
+*/
+int main(int argc, char *argv[])
+{
+int i, j, k;
+
+	if (argc != 3)
+	{
+		puts("Error");
+		return (1);
+	}
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	k = i * j;
+	printf("%d\n", k);
+	return (0);
+}
