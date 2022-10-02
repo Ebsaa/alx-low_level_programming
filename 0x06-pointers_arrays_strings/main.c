a2be38e7712c734dbee6739c236ce595e2fc7df8
Ebsaa <ebsaumar200@gmail.com>
2022-10-02 14:46:37 -0700

    task 11 103-infinite_add.c

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -9,16 +9,15 @@ int a[5];
 int *p;
 
 a[2] = 1024;
-p = &n
+p = &n;
 /*
 * write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
+* Remember
+* - you are not allowed to us
+* - you are not allowed to modify
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
-
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
