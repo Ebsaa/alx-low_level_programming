38efd4e44c7d78a253000a12d7e144513db0aa4f
Ebsaa <ebsaumar200@gmail.com>
2022-10-04 14:54:05 -0700

    task 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,41 +11,30 @@
 */
 char *argstostr(int ac, char **av)
 {
-int i, j, k, size;
-char *arg;
-size = 0;
-k = 0;
-if (ac == 0 || av == NULL)
-	return (NULL);
-i = 0;
-while (i < ac)
+int i, j, k = 0, n = 0;
+char *s;
+if (ac <= 0 || av == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
 {
-	j = 0;
-	while (av[i][j])
-	{
-		size++;
-		j++;
-	}
-	size++;
-	i++;
+for (j = 0; av[i][j]; j++)
+n++;
+n++;
 }
-arg = malloc((sizeof(char) * size)+1);
-if (arg == NULL)
-	return (NULL);
-	i = 0;
-	while (i < ac)
-	{
-	j = 0;
-	while (av[i][j])
-		{
-		arg[k] = (av[i][j]);
-		j++;
-		k++;
-		}
-	arg[k] = '\n';
-	k++;
-	i++;
-	}
-arg[k] = '\0';
-return (arg);
+n++;
+s = malloc(n *sizeof(char));
+if (s == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+s[k] = av[i][j];
+k++;
+}
+s[k] = '\n';
+k++;
+}
+s[k] = '\0';
+return (s);
 }
