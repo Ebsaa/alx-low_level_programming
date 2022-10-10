1707f87418c2f34dce67a604e9c1f7be16e67e5f
Ebsaa <ebsaumar200@gmail.com>
2022-10-10 12:47:47 -0700

    task 0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef MACRO_H
+#define MACRO_H
+
+/**
+* header file name- 0-object_like_macro.h
+* owner - preprocessor
+* Header file define macro name size abbrev of 1024 token
+*/
+#define SIZE 1024
+
+#endif
