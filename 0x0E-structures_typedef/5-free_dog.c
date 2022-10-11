13dbf193197803ae4f9f7d626da4c31907feac6b
Ebsaa <ebsaumar200@gmail.com>
2022-10-11 14:08:09 -0700

    task 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* free_dog - frees the space allocated to a dog variable
+* @d: the dog variable
+* Return: void
+*/
+void free_dog(dog_t *d)
+{
+if (d)
+	{
+	if (d->name)
+		free(d->name);
+	if (d->owner)
+		free(d->owner);
+		free(d);
+	}
+}
