57a672e8ffd9b3e957e34426e18d8e51826d0d18
Ebsaa <ebsaumar200@gmail.com>
2022-10-14 15:05:20 -0700

    task -3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -16,34 +16,34 @@ va_list args;
 char *delimiter = "", *string;
 va_start(args, format);
 len = strlen(format);
-	if (format)
-	{
-	while (format[i] && i < len)
-	{
-		switch (format[i])
-		{
-			case 'c':
-				printf("%s%c",  delimiter, va_arg(args, int));
-				break;
-			case 'i':
-				printf("%s%d", delimiter, va_arg(args, int));
-				break;
-			case 'f':
-				printf("%s%f", delimiter, va_arg(args, double));
-				break;
-			case 's':
-				string =  va_arg(args, char *);
-				if (!string)
-					printf("(nil)");
-				printf("%s%s", delimiter, string);
-				break;
-			default:
-				i++;
-				continue;
-		}
-		delimiter = ", ";
-		i++;
-	}
+if (format)
+{
+while (format[i] && i < len)
+{
+	switch (format[i])
+{
+case 'c':
+	printf("%s%c",  delimiter, va_arg(args, int));
+	break;
+case 'i':
+	printf("%s%d", delimiter, va_arg(args, int));
+	break;
+case 'f':
+	printf("%s%f", delimiter, va_arg(args, double));
+	break;
+case 's':
+	string =  va_arg(args, char *);
+	if (!string)
+		printf("(nil)");
+	printf("%s%s", delimiter, string);
+	break;
+default:
+i++;
+continue;
+}
+delimiter = ", ";
+i++;
+}
 }
 printf("\n");
 va_end(args);
